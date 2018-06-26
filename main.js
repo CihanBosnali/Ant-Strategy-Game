74d1889873d65a935b3497970a6b2e8f461ae437
Cihan Bosnalı <cihanbosna@hotmail.com>
2018-06-26 15:58:33 +0300

    No more  players! Only 2 can play after this commit

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-const startgame = function startgame() {
-	const playernum = parseInt(document.getElementById("player_number").value);
-	const players = [];
-
-	if (players.length != playernum) throw new Error("player number doesnt match the entered names");
-
-	const myUrl = new URL("/game.html");
-	myUrl.searchParams.append("pn", playernum);
-	for (let i = 0; i < players.length; i++) {
-		const playername = players[i];
-		myUrl.searchParams.append("p"+i, playername);
-	}
-
-	window.location.href = myUrl.href;
-};
-
+function startgame(){
+	
+}
