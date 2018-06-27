2c3d70160f9c2afce4d44cc776159997e7401478
Ekin Oral <oralekin@gmail.com>
2018-06-27 08:26:12 +0000

    initialize a game on javascript load

--- gameview/classes/player.js
+++ gameview/classes/player.js
@@ -4,7 +4,12 @@ export class Player extends Tile {
 	constructor(clr, id) {
 		super(0, 0);
 		this.id = id;
-		this.color = clr;
+		this.color = clr || (function (id) {
+			if (id === 0) {
+				return "#FF0000";
+			} else return "#0000FF";
+		})(id);
+		this.state = this.STATES["2"];
 	}
 
 	get start() {
@@ -23,9 +28,9 @@ export class Player extends Tile {
 	renderFunc(game) {
 		game.canvas.ctx.beginPath();
 		game.canvas.ctx.arc(
-			game.settings.tilesize * this.pos.x + 2,
-			game.settings.tilesize * this.pos.y + 2,
-			(game.settings.tilesize - 2) / 2,
+			game.settings.tilesize * this.pos.x + (game.settings.tilesize / 2) + 1,
+			game.settings.tilesize * this.pos.y + (game.settings.tilesize / 2) + 1,
+			(game.settings.tilesize - 2) / 2 - 4,
 			0, 2 * Math.PI
 		);
 		game.canvas.ctx.fillStyle = this.color;
--- gameview/main.js
+++ gameview/main.js
@@ -1,1 +1,25 @@
 import { Game } from "./classes/game.js";
+
+// get the settings from the current url
+const query = new URLSearchParams(window.location.href);
+
+// get the buttons on the page
+const buttons = {};
+buttons.restart = document.getElementById("btn_restart");
+buttons.finish = document.getElementById("btn_end");
+
+// make the restart button have the same query as the current URI
+buttons.restart.attributes.href += `?${query}`;
+
+
+const game = new Game(query.get("size") || 600, query.get("tileW") || 60);
+game.initGame("game");
+game.addPlayer(query.get("p1c"));
+game.addPlayer(query.get("p2c"));
+game.players[0].start = query.get("p1pos") ? JSON.parse(query.get("p1pos")) : {x: 2, y: 2};
+game.players[1].start = query.get("p2pos") ? JSON.parse(query.get("p2pos")) : {x: 7, y: 2};
+game.initTiles().then(game => {
+	game.render();
+	console.log(game);
+	game.tiles[0][5].render(game);
+});
